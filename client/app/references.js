f7faa94126ead15b1cd5980e8b6a8a13f697a508
dan <nixon.daniel.j@gmail.com>
2014-08-17 17:04:30 -0400

    registration works, don't trust the last few commits...

---
+++ client/app/references.js
@@ -0,0 +1,4 @@
+﻿/// <reference path="core/angular.js" />
+/// <reference path="core/app.js" />
+/// <reference path="controllers/registrationCtrl.js" />
+/// <reference path="services/userSvc.js" />
